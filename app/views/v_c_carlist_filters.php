1e64980fac0a94db850e447a0e49246b92ebb455
Yadigar ZENGİN <yadigarbz@gmail.com>
2018-02-02 12:46:49 +0300

    Inıtal

--- app/views/v_c_carlist_filters.php
+++ app/views/v_c_carlist_filters.php
@@ -1,68 +1,74 @@
 <div class="col-lg-3 col-sm-4 col-xs-12">
     <aside class="b-items__aside">
-        <h2 class="s-title wow zoomInUp" data-wow-delay="0.5s">REFINE YOUR SEARCH</h2>
+        <h2 class="s-title wow zoomInUp" data-wow-delay="0.5s">Aramanızı Filtreleyin</h2>
         <div class="b-items__aside-main wow zoomInUp" data-wow-delay="0.5s">
             <form>
                 <div class="b-items__aside-main-body">
                     <div class="b-items__aside-main-body-item">
-                        <label>SELECT A MAKE</label>
+                        <label>Marka Seçin</label>
                         <div>
                             <select name="select1" class="m-select">
-                                <option value="" selected="">Any Make</option>
+                                <option value="" selected=""> Fark Etmez </option>
+                                <option value="" > Aston Martin </option>
                             </select>
                             <span class="fa fa-caret-down"></span>
                         </div>
                     </div>
                     <div class="b-items__aside-main-body-item">
-                        <label>SELECT A MODEL</label>
+                        <label>Model Seçin</label>
                         <div>
                             <select name="select1" class="m-select">
-                                <option value="" selected="">Any Make</option>
+                                <option value="" selected=""> Fark Etmez </option>
                             </select>
                             <span class="fa fa-caret-down"></span>
                         </div>
                     </div>
                     <div class="b-items__aside-main-body-item">
-                        <label>PRICE RANGE</label>
+                        <label> Fiyat Aralığı </label>
                         <div class="slider"></div>
                         <input type="hidden" name="min" value="100" class="j-min" />
                         <input type="hidden" name="max" value="1000" class="j-max" />
                     </div>
                     <div class="b-items__aside-main-body-item">
-                        <label>VEHICLE TYPE</label>
+                        <label> Araç Türü </label>
                         <div>
                             <select name="select1" class="m-select">
-                                <option value="" selected="">Any Type</option>
+                                <option value="" selected=""> Fark Etmez </option>
+                                <option value=""> SUV </option>
                             </select>
                             <span class="fa fa-caret-down"></span>
                         </div>
                     </div>
                     <div class="b-items__aside-main-body-item">
-                        <label>VEHICLE STATUS</label>
+                        <label> Araç Durumu </label>
                         <div>
                             <select name="select1" class="m-select">
-                                <option value="" selected="">Any Status</option>
+                                <option value="" selected=""> Fark Etmez </option>
+                                <option value=""> 2. El </option>
                             </select>
                             <span class="fa fa-caret-down"></span>
                         </div>
                     </div>
                     <div class="b-items__aside-main-body-item">
-                        <label>FUEL TYPE</label>
+                        <label> Yakıt Tipi </label>
                         <div>
                             <select name="select1" class="m-select">
-                                <option value="" selected="">All Fuel Types</option>
+                                <option value="" selected=""> Fark Etmez </option>
+                                <option value=""> Benzinli </option>
+                                <option value=""> Dizel </option>
+                                <option value=""> LPG </option>
                             </select>
                             <span class="fa fa-caret-down"></span>
                         </div>
                     </div>
                 </div>
                 <footer class="b-items__aside-main-footer">
-                    <button type="submit" class="btn m-btn">FILTER VEHICLES<span class="fa fa-angle-right"></span></button><br />
-                    <a href="">RESET ALL FILTERS</a>
+                    <button type="submit" class="btn m-btn"> Filtreleri Uygula <span class="fa fa-angle-right"></span></button><br />
+                    <a href=""> Filtreleri Sıfırla </a>
                 </footer>
             </form>
         </div>
-        <div class="b-items__aside-sell wow zoomInUp" data-wow-delay="0.5s">
+        <!--<div class="b-items__aside-sell wow zoomInUp" data-wow-delay="0.5s">
             <div class="b-items__aside-sell-img">
                 <h3>SELL YOUR CAR</h3>
             </div>
@@ -74,6 +80,6 @@
                 </p>
                 <a href="submit1.html" class="btn m-btn">REGISTER NOW<span class="fa fa-angle-right"></span></a>
             </div>
-        </div>
+        </div>-->
     </aside>
 </div>
--- app/views/v_c_minibanner.php
+++ app/views/v_c_minibanner.php
@@ -1,8 +1,8 @@
 <section class="b-pageHeader">
     <div class="container">
-        <h1 class=" wow zoomInLeft" data-wow-delay="0.5s">Auto Listings</h1>
+        <h1 class=" wow zoomInLeft" data-wow-delay="0.5s">Arabalar</h1>
         <div class="b-pageHeader__search wow zoomInRight" data-wow-delay="0.5s">
-            <h3>Your search returned 28 results</h3>
+            <h3>Aramanıza ait <span style="font-weight: bold">28</span> sonuç bulundu.</h3>
         </div>
     </div>
 </section><!--b-pageHeader-->
